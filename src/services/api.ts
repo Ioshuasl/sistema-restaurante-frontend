5187fdf2cea357c4f640d27b355affdad53cea4a
= <=>
2025-10-18 08:56:30 -0300

    arrumando conexao com o backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://projeto-backend-restaurante.lwcbm0.easypanel.host/api',
   headers: {
     'Content-Type': 'application/json',
   },
