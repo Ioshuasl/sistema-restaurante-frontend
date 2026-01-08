bb5fbb0d282bc82af0d920de07fcf9209b84dd3b
= <=>
2026-01-08 16:25:35 -0300

    adicionando mais personalizacao do cardapio

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://projeto-backend-restaurante.lwcbm0.easypanel.host/api',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true'
